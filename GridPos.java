0d52cf5137b001c532d0aadbb745143bf0d70601
Lucas Holter <holter@chalmers.se>
2022-11-17 03:07:00 +0100

    Added updatePos in Snake class and move in gridPos class.

--- GridPos.java
+++ GridPos.java
@@ -6,4 +6,28 @@ public class GridPos {
     this.xPos = xPos;
     this.yPos = yPos;
   }
+
+  public void move(int direction){
+    switch (direction) {
+      case 0:
+        this.yPos -= 1; 
+      break;
+
+      case 1:
+        this.xPos -= 1; 
+      break;
+
+      case 2:
+        this.yPos += 1; 
+      break;
+
+      case 3:
+        this.xPos += 1; 
+      break;
+
+      default:
+        //Error
+      break;
+    }
+  }
 }
--- Snake.java
+++ Snake.java
@@ -3,9 +3,18 @@ import java.util.ArrayList;
 public class Snake {
   private GridPos headPos;
   private ArrayList<GridPos> bodyPos;
+  private int direction; // 0 = north, 1 = west, 2 = south, 3 = east
 
   public Snake(){
-    headPos = new GridPos(1,1);
-    bodyPos = new ArrayList<>();
+    this.headPos = new GridPos(5,5);
+    this.bodyPos = new ArrayList<>();
+    this.direction = 2;
+  }
+
+  public void updatePos(){
+    for(int i = 0; ){
+
+    }
+     headPos.move(direction); 
   }
 }
