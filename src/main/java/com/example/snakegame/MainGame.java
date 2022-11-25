4394451080c486fb3726f8f34773ca3b2b01ae15
Felix Humleby <gushumfe@student.gu.se>
2022-11-25 13:06:12 +0100

    Added frontend javafx

---
+++ src/main/java/com/example/snakegame/MainGame.java
@@ -0,0 +1,25 @@
+package com.example.snakegame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainGame extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("GameScene.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Snake: The Game");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
