05a096dd11012297a2b285f787b0b1709e909e98
Felix Humleby <gushumfe@student.gu.se>
2022-11-25 14:49:57 +0100

    Added a grid and made samll squeres

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,8 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,12 +13,26 @@ public class MainGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("resources/StartMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/GameScene.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Backend.Snake: The Backend.Game");
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
+
+        Canvas grid = (Canvas) scene.lookup("#Grid");
+        GraphicsContext gc = grid.getGraphicsContext2D();
+        for( int i = 0; i < 20; i++){
+            for(int j = 0; j < 20; j++){
+                if((i+j) % 2 == 0){
+                    gc.setFill(Color.BLACK);
+                }else{
+                    gc.setFill(Color.WHITE);
+                }
+                gc.fillRect(i * 50, j * 50, 50,50);
+            }
+        }
+
 
     }
 
